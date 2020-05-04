e5842db49a67413769e30ec51767d18a8caf480b
Vini3x3 <vincent.hanghang@gmail.com>
2020-05-04 11:32:22 +0800

    Update testground.py

--- testground.py
+++ testground.py
@@ -9,43 +9,57 @@ credential = {
 webscrape_settings = {
     # 'browser': 'Edge',
     # 'headless': False,
-    # 'verbose': 3,
+    'verbose': 3,
     # 'initialize-website': 'All',
 }
+
 
 @testsuit.log('field', 'testground')
 @testsuit.errorTest
 def testground(webmaster):
-    result = webmaster.query('Moodle','getSiteMap')
-    for _ in result:
-        print(_)
+    # result = webmaster.query('Moodle','getSiteMap')
+    # for _ in result:
+    #     print(_)
     result = webmaster.query('Moodle','findAllCoursesByKeywords', 'comp')
-    for _ in result:
-        print(_)
-
+    # for _ in result:
+    #     print(_)
+    #
     result = webmaster.query('Moodle', 'scrapeCourseContents', 'https://moodle.hku.hk/course/view.php?id=43519')
     print(result)
-    result = webmaster.query('Moodle', 'scrapeCourseContentPreview', 'https://moodle.hku.hk/mod/assign/view.php?id=1738380')
-    print(result)
-    result = webmaster.query('Portal', 'findInvoice')
-    for key in result.keys():
-        print(key)
-    result = webmaster.query('Portal', 'findTranscript')
-    for key in result.keys():
-        print(key)
-    result = webmaster.query('Portal', 'findAccountActivity')
-    for key in result.keys():
-        print(key)
-    result = webmaster.query('Portal', 'findReceipt')
-    for key in result.keys():
-        print(key)
-    result = webmaster.query('Portal', 'findWeeklySchedule', '25/01/2020', '8:00AM', '11:00PM')
-    print(result)
+    # result = webmaster.query('Moodle', 'scrapeCourseContentPreview', 'https://moodle.hku.hk/mod/assign/view.php?id=1738380')
+    # print(result)
+    # result = webmaster.query('Moodle', 'scrapeDeadlines')
+    # for _ in result: print(_)
+    # result = webmaster.query('Portal', 'findInvoice')
+    # for key in result.keys():
+    #     print(key)
+    # print('hi')
+    # result = webmaster.query('Portal', 'findTranscript')
+    # for key in result.keys():
+    #     print(key)
+    # print('hi')
+    # result = webmaster.query('Portal', 'findAccountActivity')
+    # for key in result.keys():
+    #     print(key)
+    # print('hi')
+    # result = webmaster.query('Portal', 'findReceipt')
+    # for key in result.keys():
+    #     print(key)
+    # print('hi')
+    # result = webmaster.query('Portal', 'findWeeklySchedule', '25/01/2020', '8:00AM', '11:00PM')
+    # print(result)
+    # result = webmaster.query('Portal', 'findWeeklySch', '25/01/2020', '8:00AM', '11:00PM')
+    # print(len(result))
+    # result = webmaster.query('Moodle', 'findCourseContent', 'Implementation', ': ', ['File'], -1, False, False)
+    # for _ in result: print(_)
+
+    # result = webmaster.query('Portal', 'findTable', ['GRID_GPA', 'Z_BILLPAYH2_TBL', 'TOTAL_DUE'])
+    # for key in result:
+    #     print(key)
 
 
 # webmaster = WebMaster(credential, webscrape_settings)
 webmaster = WebMaster(credential)
 testground(webmaster)
 webmaster.terminateThread()
-
 
