68332e1b425cea653ad7fbfe1f32fa657045b9d4
Vini3x3 <vincent.hanghang@gmail.com>
2020-05-04 23:29:37 +0800

    Update testground.py

--- testground.py
+++ testground.py
@@ -1,10 +1,13 @@
 from myutil.testsuit import testsuit3 as testsuit
 from myscraper.webmaster import WebMaster
+from time import sleep
+from tabulate import tabulate
 
 credential = {
     'username': '',
     'password': '',
 }
+
 webscrape_settings = {
     # 'browser': 'Edge',
     # 'headless': False,
@@ -23,11 +26,11 @@ def testground(webmaster):
     print(result)
     result = webmaster.query('Moodle', 'find_all_courses_by_keywords', 'comp')
     for _ in result: print(_)
-    result = webmaster.query('Moodle', 'scrape_course_content', 'https://moodle.hku.hk/course/view.php?id=43519')
+    result = webmaster.query('Moodle', 'find_course_contents', 'https://moodle.hku.hk/course/view.php?id=43519')
     for _ in result: print(_)
     result = webmaster.query('Moodle', 'scrape_course_preview', 'https://moodle.hku.hk/mod/assign/view.php?id=1738380')
     print(result)
-    result = webmaster.query('Moodle', 'scrape_deadlines')
+    result = webmaster.query('Moodle', 'find_deadlines')
     for _ in result: print(_)
     result = webmaster.query('Portal', 'find_invoice')
     for key in result.keys():
@@ -47,10 +50,38 @@ def testground(webmaster):
     print(result)
 
 
-# webmaster = WebMaster(credential, webscrape_settings)
-webmaster = WebMaster(credential['username'], credential['password'])
-testground(webmaster)
-webmaster.cancel()
 
+@testsuit.timedTest(repeat=1)
+def test_1(webmaster):
+    result = webmaster.query('Moodle', 'find_course_contents', 'crime investigation')
+    # result = webmaster.query('Moodle', 'find_deadlines')
+    print(len(result))
+
+# webmaster = WebMaster(credential, webscrape_settings)
+webmaster = WebMaster(credential['username'], credential['password'], verbose=0)
+# testground(webmaster)
+
+# create WebMaster
+# webmaster = WebMaster('username', 'password')
+
+# use WebMaster
+# result = webmaster.query('Moodle', 'find_deadlines')
+# print(test_1(webmaster))
+result = webmaster.query('Moodle', 'find_course_contents',
+                         'Computer and Communication Networks', ['URL', 'Assignment'], search=[['problem-set']], quota=-2
+                         )
+# print(len(result))
+# result = webmaster.query('Moodle', 'find_deadlines')
+# result = webmaster.query('Moodle', 'test', 'https://moodle.hku.hk/course/view.php?id=43519')
+
+# result = webmaster.query('Portal', 'find_weekly_sch', '25/04/2020')
+# print(result)
+print(tabulate(result, headers='keys'))
+
+# get WebMaster history
+records = webmaster.get_record()
+
+# not in use
+webmaster.cancel()
 
 
